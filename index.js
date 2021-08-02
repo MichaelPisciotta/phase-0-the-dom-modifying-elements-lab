1e43b567c05137c34df57c8925ae43280e97b751
MichaelPisciotta <mfp56@miami.edu>
2021-08-02 18:50:37 -0400

    commit tree bd257d03ff47661a097eaeba9fc6259667ecae6f

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 // Write your code here!
+
+
 main.remove();
 
 const newHeader = document.getElementById('victory');
 
-const header = document.getElementById('victory');
+// const header = document.getElementById('victory');
+
+
